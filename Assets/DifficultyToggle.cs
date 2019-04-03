52bcbd5aff1ef9e3f41dbd7383d02cd609538ba6
DESKTOP-4028UN2\greenguitarhero <coolsouro@gmail.com>
2019-04-03 22:16:02 +0530

    Difficulty settings added along with implementation of difficulty toggling.

---
+++ Assets/DifficultyToggle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyToggle : MonoBehaviour
+{
+    private Toggle[] toggles = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggles = gameObject.GetComponentsInChildren<Toggle>();
+        int currentDifficulty = Settingmanager.Instance.difficulty;
+        if (currentDifficulty == 0)
+            SetToggle("toggleeasy");
+        else if (currentDifficulty == 1)
+            SetToggle("togglenormal");
+        else
+            SetToggle("togglehard");
+    }
+
+    void SetToggle(string tname)
+    {
+        foreach (Toggle toggle in toggles)
+        {
+            if (toggle.name == tname)
+            
+                toggle.isOn = true;
+            
+            else
+                toggle.isOn = false;
+        }
+    }
+    void Update()
+    { }
+    // Update is called once per frame
+    public void Updatedifficulty()
+    {
+        foreach(Toggle toggle in toggles)
+        {
+            if (toggle.isOn)
+            {
+                if (toggle.name == "toggleeasy") 
+                Settingmanager.Instance.difficulty = 0;
+                else if (toggle.name== "togglenormal")
+                    Settingmanager.Instance.difficulty = 1;
+                else
+                    Settingmanager.Instance.difficulty = 2;
+            }
+        }
+    }
+}
